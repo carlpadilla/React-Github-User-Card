929c9fbf5f5a8bd84a393b7541d3685c3681916f
carlpadilla <carlpadilla@protonmail.com>
2019-09-06 01:24:16 -0400

    added constructor and state

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -3,9 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
-  // constructor() {
-  //   super();
-  // }
+  constructor() {
+    super();
+
+    this.state = {
+      user: []
+    };
+  }
   render() {
     return (
       <div className='App'>
